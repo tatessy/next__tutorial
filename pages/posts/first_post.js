0564d37dab5fd9bec9811853c64999d7c9211d83
tateshi <yarumaya0822@gmail.com>
2022-08-26 02:25:07 +0900

    tutorial 3回目まで

---
+++ pages/posts/first_post.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Layout from '../../components/layout'
+
+export default function FirstPost() {
+    return (
+        <Layout>
+        <Head><title>first post</title></Head>
+        <h1>最初のポスト</h1>
+        <h2><Link href = '/'><a>back to home</a></Link></h2>
+        </Layout>
+    )
+}
