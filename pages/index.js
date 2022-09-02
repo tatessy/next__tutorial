284c1d2b168cbb0d7593758ee836eb9a9d42f631
tateshi <yarumaya0822@gmail.com>
2022-09-02 23:45:08 +0900

    add: lesson5: 動的ルーティング後半

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
+
 
 export default function Home({ allPostsData }) {
   return (
@@ -31,7 +34,17 @@ export default function Home({ allPostsData }) {
           ))}
         </ul>
       </section>
+      <li className={utilStyles.listItem} key={id}>
+  <Link href={`/posts/${id}`}>
+    <a>{title}</a>
+  </Link>
+  <br />
+  <small className={utilStyles.lightText}>
+    <Date dateString={date} />
+  </small>
+</li>
     </Layout>
+    
   )}
 
 
